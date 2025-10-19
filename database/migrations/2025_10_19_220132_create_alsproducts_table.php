eabf211eff74b2315cb4f7912444ff6edf6edce5
Andy <gabryelb027@gmail.com>
2025-10-19 17:16:57 -0500

    Primer commit Laravel

---
+++ database/migrations/2025_10_19_220132_create_alsproducts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alsproducts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price', 10, 2); 
+            $table->integer('stock');
+            $table->foreignId('product_category_id')->constrained('alsproduct_categories')->onDelete('cascade');
+            $table->foreign('supllier_id')->constrained('alssuppliers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alsproducts');
+    }
+};
